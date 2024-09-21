2fc4117db45e486de4fd6106a9754c3cf3e55157
Petani Kode <ijankmugello@gmail.com>
2024-09-21 15:37:34 +0800

    membuat MVC Bank

---
+++ app/Models/Bank.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bank extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function($query, $search){
+            return $query->where(function($query) use ($search){
+                $query->where('name','like','%' . $search . '%')
+                       ->orWhere('slug','like','%' . $search . '%');
+            });
+
+        });
+    }
+}
